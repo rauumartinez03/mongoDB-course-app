49560525e53a0e0dbe5a7a4b16ccdaac1d274af5
rauumartinez03 <raul.us03@gmail.com>
2024-02-10 20:22:50 +0100

    Creada aplicación React + NodeJs + MongoDB

---
+++ backend/app.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongodb = require('mongodb').MongoClient;
+
+const productRoutes = require('./routes/products');
+const authRoutes = require('./routes/auth');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use('/images', express.static(path.join('backend/images')));
+
+app.use((req, res, next) => {
+  // Set CORS headers so that the React SPA is able to communicate with this server
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,PUT,PATCH,DELETE,OPTIONS'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+});
+
+app.use('/products', productRoutes);
+app.use('/', authRoutes);
+
+mongodb.connect('mongodb+srv://appdev:dev@project0.opucdzh.mongodb.net/?retryWrites=true&w=majority')
+
+app.listen(3100);
