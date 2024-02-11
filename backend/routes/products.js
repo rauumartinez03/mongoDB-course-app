fa210a4c7478bae5f71243f1c52365a0359f27af
rauumartinez03 <raul.us03@gmail.com>
2024-02-11 21:34:14 +0100

    fetch data from database

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -57,18 +57,42 @@ const products = [
 
 // Get list of products products
 router.get('/', (req, res, next) => {
-  // Return a list of dummy products
-  // Later, this data will be fetched from MongoDB
-  const queryPage = req.query.page;
-  const pageSize = 5;
-  let resultProducts = [...products];
-  if (queryPage) {
-    resultProducts = products.slice(
-      (queryPage - 1) * pageSize,
-      queryPage * pageSize
-    );
-  }
-  res.json(resultProducts);
+  // const queryPage = req.query.page;
+  // const pageSize = 5;
+  // let resultProducts = [...products];
+  // if (queryPage) {
+  //   resultProducts = products.slice(
+  //     (queryPage - 1) * pageSize,
+  //     queryPage * pageSize
+  //   );
+  // }
+  MongoClient.connect(
+    'mongodb+srv://appdev:dev@project0.opucdzh.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      const products = [];
+      client
+        .db()
+        .collection('products')
+        .find()
+        .forEach(productDoc => {
+          productDoc.price = productDoc.price.toString();
+          products.push(productDoc);
+        })
+        .then(result => {
+          client.close();
+          res.status(200).json(products);
+        })
+        .catch(err => {
+          console.log(err);
+          client.close();
+          res.status(500).json({ message: 'An error occurred.' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'An error occurred.' });
+    });
 });
 
 // Get single product
@@ -86,24 +110,31 @@ router.post('', (req, res, next) => {
     price: Decimal128.fromString(req.body.price.toString()), // store this as 128bit decimal in MongoDB
     image: req.body.image
   };
-
-  MongoClient.connect('mongodb+srv://appdev:dev@project0.opucdzh.mongodb.net/shop?retryWrites=true&w=majority')
-  .then((client) => {
-    client.db().collection('products').insertOne(newProduct)
-      .then(res => {
-        console.log(res);
-        client.close();
-        res.status(201).json({ message: 'Product added', productId: res.insertedId });
-      })
-      .catch(err => {
-        console.log(err);
-        client.close();
-        res.status(500).json({ message: 'An error ocurred'});
-      });
-  })
-  .catch(err => {
-    console.log(err)
-  });
+  MongoClient.connect(
+    'mongodb+srv://appdev:dev@project0.opucdzh.mongodb.net/shop?retryWrites=true&w=majority'
+  )
+    .then(client => {
+      client
+        .db()
+        .collection('products')
+        .insertOne(newProduct)
+        .then(result => {
+          console.log(result);
+          client.close();
+          res
+            .status(201)
+            .json({ message: 'Product added', productId: result.insertedId });
+        })
+        .catch(err => {
+          console.log(err);
+          client.close();
+          res.status(500).json({ message: 'An error occurred.' });
+        });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ message: 'An error occurred.' });
+    });
 });
 
 // Edit existing product
